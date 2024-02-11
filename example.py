0fbb3833dcb8673c143470b98ff2e188aa86995a
Saeed Misaghian (SaM) <78544726+SaM-92@users.noreply.github.com>
2024-02-11 20:41:30 +0000

    create environment and requriement files

--- example.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "https://api-access.electricitymaps.com/free-tier/home-assistant"
-headers = {"auth-token": "8YB3GhYHdn6DGVoKEIV7rCxBvdEW5FNF"}
-
-response = requests.get(url, headers=headers)
-
-print(response.text)
